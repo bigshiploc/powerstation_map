df34974458fe2ab9897a3eeeebde9ebdcfd9c822
liuchaoxue <482739566@qq.com>
2017-12-28 18:27:05 +0800

    数据发送

--- public/javascripts/3d_map.js
+++ public/javascripts/3d_map.js
@@ -20,12 +20,10 @@
 
     //faye接收数据
     var client = new Faye.Client('http://localhost:3000/faye');
-	client.subscribe('/data', function(msg) {
-        message= msg;
+    client.subscribe('/data', function (msg) {
+        message = msg;
         fayeMsg(msg)
-	});
-
-
+    });
 
 
     //获取唯一id
@@ -39,11 +37,11 @@
     //绘制标志的方法
     function addOverlay(msg) {
         var piont = map.unoffset(msg.piont.substring(0, msg.piont.indexOf(',')), msg.piont.substring(msg.piont.indexOf(',') + 1));
-        if(document.getElementById(msg.sbms)===null){
+        if (document.getElementById(msg.sbms) === null) {
             console.log('==清除标记循环==');
             var overlay = map.addOverlay({
                 url: '../images/search_marker.png',  //标志样式的存放地址
-                position: [piont.x+Math.random()*100, piont.y+Math.random()*10],  //标志要添加的位置坐标
+                position: [piont.x + Math.random() * 100, piont.y + Math.random() * 10],  //标志要添加的位置坐标
                 size: [32, 32],  //标志的大小
                 anchor: [0, 0],
                 className: 'overlay-icon',
@@ -52,22 +50,22 @@
             overlay.targetDom.id = msg.sbms;
             allOverlay[msg.sbms] = overlay;
             overlay.on("click", function (e) {
-                layer.tips('<div>'+msg.piont +'</div>', '#' + e.targetDom.id, {
+                layer.tips('<div>' + msg.piont + '</div>', '#' + e.targetDom.id, {
                     area: 'auto',
                     maxWidth: '500px',
                     skin: 'liu-tips-class',
                     time: false,
                     closeBtn: 1,
                     tips: [1, 'white'],
-                    id : e.targetDom.id+"tips"
+                    id: e.targetDom.id + "tips"
                 });
             })
-        }else {
-            if(document.getElementById(msg.sbms+"tips")!=null){
+        } else {
+            if (document.getElementById(msg.sbms + "tips") != null) {
                 console.log(msg.piont);
-                document.getElementById(msg.sbms+"tips").getElementsByTagName('div')[0].innerText=allOverlay[msg.sbms].position
+                document.getElementById(msg.sbms + "tips").getElementsByTagName('div')[0].innerText = allOverlay[msg.sbms].position
             }
-            allOverlay[msg.sbms].position = {x:piont.x+Math.random()*100, y:piont.y+Math.random()*10};
+            allOverlay[msg.sbms].position = {x: piont.x + Math.random() * 100, y: piont.y + Math.random() * 10};
             clearInterval(AddOverlay);
         }
 
@@ -110,16 +108,16 @@
     }
 
     function fayeMsg(msg) {
-            if(msg[i].sbms==='设备描述1') {
-                tryAddOverlay(msg[i])
-            }
+        if (msg[i].sbms === '设备描述1') {
+            tryAddOverlay(msg)
+        }
 
     }
 
-    function tryAddOverlay(msg,qyms) {
-                AddOverlay = setInterval(function () {
-                    addOverlay(msg);
-                }, 1000)
+    function tryAddOverlay(msg, qyms) {
+        AddOverlay = setInterval(function () {
+            addOverlay(msg);
+        }, 1000)
     }
 
     var BuildingControl = setInterval(function () {
@@ -149,24 +147,23 @@
         };
     };
 
-	//给地图添加点击事件（以后要做点击建筑物或终端显示其基本信息）
-	map.onClick = function (e) {
-		var feature = e.feature;
+    //给地图添加点击事件（以后要做点击建筑物或终端显示其基本信息）
+    map.onClick = function (e) {
+        var feature = e.feature;
 
-		console.log(e);
-		if (feature.parent.name === 'Area') {
-			console.log('--这里是一个回调--');
+        console.log(e);
+        if (feature.parent.name === 'Area') {
+            console.log('--这里是一个回调--');
             layer.alert(JSON.stringify(e.point), {
                 skin: 'layui-layer-molv' //样式类名
-                ,closeBtn: 0
+                , closeBtn: 0
             });
-			map.setColor(feature.parent, 'id', feature.id, 0xff0000);
-		}
-	};
+            map.setColor(feature.parent, 'id', feature.id, 0xff0000);
+        }
+    };
 
 
-
-	//添加地图加载完成时的回调
+    //添加地图加载完成时的回调
     // map.onLoad = function () {
     //
     //     var arr = [[14100500.629299998, 5708193.7654], [14100495.503700003, 5708153.087099999], [14100440.729900002, 5708156.914000001], [14100374.820500001, 5708174.3869]];
